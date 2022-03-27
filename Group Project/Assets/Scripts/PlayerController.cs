ab232d6f3f0cfe55d8da366c88ba7570fb4e1c47
GeirlanthArchivest <100598359+GeirlanthArchivest@users.noreply.github.com>
2022-03-27 17:25:17 +0100

    Added the project with test player and movement

---
+++ Group Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private float speed = 20;
+    private float turnSpeed = 20;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);  
+
+    }
+}
