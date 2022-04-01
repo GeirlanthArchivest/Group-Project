728004a1dcfa834b81ebc13951da2e828d62ab12
GeirlanthArchivest <100598359+GeirlanthArchivest@users.noreply.github.com>
2022-04-01 15:11:43 +0100

    had help to fix rigidbody movement and collisions

--- Group Project/Assets/Scripts/PlayerController.cs
+++ Group Project/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 	private float speed = 20;
-    private float turnSpeed = 50;
-    private float horizontalInput;
-    private float forwardInput;
+	private float turnSpeed = 50;
+	private float horizontalInput;
+	private float forwardInput;
 	public int maxHealth = 100;
 	public int currentHealth;
 	
@@ -29,9 +29,11 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
-        //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+		//Move the vehicle forward
+		transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+		Vector3 forwardDirection = transform.rotation * Vector3.forward;
+		GetComponent<Rigidbody>().velocity = forwardDirection * speed * forwardInput;
+        //transform.Translate(Vector3.forward * speed * forwardInput);
 		
 		if(Input.GetKeyDown(KeyCode.Space))
 		{
