3fa5d3a560a91098219495a85f97e68be1215928
GeirlanthArchivest <100598359+GeirlanthArchivest@users.noreply.github.com>
2022-04-01 13:14:23 +0100

    added healthbar code

--- Group Project/Assets/Scripts/PlayerController.cs
+++ Group Project/Assets/Scripts/PlayerController.cs
@@ -8,11 +8,16 @@ public class PlayerController : MonoBehaviour
     private float turnSpeed = 50;
     private float horizontalInput;
     private float forwardInput;
-
+	public int maxHealth = 100;
+	public int currentHealth;
+	
+	public Healthbar Healthbar;
 	
     // Start is called before the first frame update
     void Start()
     {
+		currentHealth = maxHealth;
+		Healthbar.SetMaxHealth(maxHealth);
 
     }
     
@@ -27,5 +32,18 @@ public class PlayerController : MonoBehaviour
         //Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+		
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			TakeDamage(20);
+		}
+
     }
+	
+	void TakeDamage(int damage)
+	{
+		currentHealth -= damage;
+		Healthbar.SetHealth(currentHealth);
+	}
+
 }
