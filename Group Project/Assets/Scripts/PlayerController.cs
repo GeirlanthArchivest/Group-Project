082f07993b8915bbc20d343db52ad4077761c01d
GeirlanthArchivest <100598359+GeirlanthArchivest@users.noreply.github.com>
2022-04-18 16:02:23 +0100

    got rotate but not up movement

--- Group Project/Assets/Scripts/PlayerController.cs
+++ Group Project/Assets/Scripts/PlayerController.cs
@@ -6,10 +6,8 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-	private float speed = 20;
-	private float turnSpeed = 50;
-	private float horizontalInput;
-	private float forwardInput;
+	public float speed = 20;
+	public float turnSpeed = 50;
 	public int maxHealth = 100;
 	public int currentHealth;
 	public int maxStamina = 100;
@@ -32,12 +30,14 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //get player input
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float forwardInput = Input.GetAxis("Vertical");
+        float pitchInput = Input.GetAxis("Pitch");
 
-		//Move the vehicle forward
-		transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
-		Vector3 forwardDirection = transform.rotation * Vector3.forward;
+        //Move the vehicle forward
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        transform.Rotate(Vector3.right, turnSpeed * pitchInput * Time.deltaTime);
+        Vector3 forwardDirection = transform.rotation * Vector3.forward;
 		GetComponent<Rigidbody>().velocity = forwardDirection * speed * forwardInput;
         //transform.Translate(Vector3.forward * speed * forwardInput);
 		
