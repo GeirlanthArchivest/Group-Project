ae775e59b4639504725ba31f17b3d42ac4b5fb72
GeirlanthArchivest <100598359+GeirlanthArchivest@users.noreply.github.com>
2022-06-12 01:45:49 +0100

    fixed outstanding issues

--- Group Project/Assets/Scripts/settingsMenu.cs
+++ Group Project/Assets/Scripts/settingsMenu.cs
@@ -32,6 +32,12 @@ public class settingsMenu : MonoBehaviour
 
     }
 
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
